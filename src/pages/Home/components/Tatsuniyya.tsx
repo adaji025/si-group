63d4e0451e4474656be36a3404f34a39743178c6
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2024-05-20 07:31:26 +0100

    made ui fixes

--- src/pages/Home/components/Tatsuniyya.tsx
+++ src/pages/Home/components/Tatsuniyya.tsx
@@ -13,7 +13,10 @@ const Tatsuniyya = () => {
       className="app-width flex flex-col md:flex-row justify-between items-center gap-20 mt-20"
       data-aos="fade-up"
     >
-      <div className="flex-1">
+      <div className="flex-1 order-2 md:order-1">
+        <img src={SubsidiaryImage} alt="" />
+      </div>
+      <div className="flex-1 order-1 md:order-2">
         <h2 className="font-bold text-[32px] lg:text-[48px] lg:w-1/2">
           Tattsuniyya Network{" "}
         </h2>
@@ -27,9 +30,6 @@ const Tatsuniyya = () => {
         <Button size="md" className="bg-black mt-5 hover:bg-black hover:scale-105 duration-300">
           Visit our website
         </Button>
-      </div>
-      <div className="flex-1">
-        <img src={SubsidiaryImage} alt="" />
       </div>
     </div>
   );
